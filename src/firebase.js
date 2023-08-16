7ad15cf8bad928ecfda1b78de3c8985d26cb00bf
SHIVENDRA8004 <jat13632@gmail.com>
2023-08-17 01:20:15 +0530

    Error: An error occured in installing depedencies

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
     apiKey: "AIzaSyCoILFjg1FS7IrP85Fly9lbKu9QTkvG3GI",
     authDomain: "drive-project-d5b85.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
     messagingSenderId: "312642396494",
     appId: "1:312642396494:web:610eacadd7cdbe55ff7e1f",
 };
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+const storage = getStorage();
+const db = getFirestore();
+
+export { auth, provider, db, storage };
