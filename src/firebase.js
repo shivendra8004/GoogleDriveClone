d1c494fca4ff0b1a0884bae92edf1db61fe7cb9a
SHIVENDRA8004 <jat13632@gmail.com>
2023-08-17 00:33:42 +0530

    New: Connecting firebase to add feature of storing files

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCoILFjg1FS7IrP85Fly9lbKu9QTkvG3GI",
+    authDomain: "drive-project-d5b85.firebaseapp.com",
+    projectId: "drive-project-d5b85",
+    storageBucket: "drive-project-d5b85.appspot.com",
+    messagingSenderId: "312642396494",
+    appId: "1:312642396494:web:610eacadd7cdbe55ff7e1f",
+};
