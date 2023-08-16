0c5b76382136c778ffccc3eb27a42797b2240c15
SHIVENDRA8004 <jat13632@gmail.com>
2023-08-17 00:18:17 +0530

    Feat: Sidebar done

--- src/components/sidebar/NewFile.js
+++ src/components/sidebar/NewFile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AddIcon from "@mui/icons-material/Add";
+import "../../styles/NewFile.css";
 const NewFile = () => {
     return (
         <div className="newFile">
