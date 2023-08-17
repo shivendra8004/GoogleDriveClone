38ee64c646d6d2586886f85b798f76a324fa2f5b
SHIVENDRA8004 <jat13632@gmail.com>
2023-08-17 11:30:51 +0530

    Error: Removed all imports where error is incoming

--- src/components/sidebar/NewFile.js
+++ src/components/sidebar/NewFile.js
@@ -1,32 +1,33 @@
 import React, { useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
-import { storage, db } from "../../firebase";
-
-import { makeStyles } from "@mui/styles";
-import Modal from "@mui/material";
 import "../../styles/NewFile.css";
 
-function getModalStyle() {
-    return {
-        top: `50%`,
-        left: `50%`,
-        transform: `translate(-50%,-50%)`,
-    };
-}
-const useStyles = makeStyles((theme) => ({
-    paper: {
-        position: "absolute",
-        width: 400,
-        backgroundColor: theme.palette.background.paper,
-        border: `2px solid #000`,
-        boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
-    },
-}));
+// import { storage, db } from "../../firebase";
+
+// import { makeStyles } from "@mui/styles";
+// import Modal from "@mui/material";
+
+// function getModalStyle() {
+//     return {
+//         top: `50%`,
+//         left: `50%`,
+//         transform: `translate(-50%,-50%)`,
+//     };
+// }
+// const useStyles = makeStyles((theme) => ({
+//     paper: {
+//         position: "absolute",
+//         width: 400,
+//         backgroundColor: theme.palette.background.paper,
+//         border: `2px solid #000`,
+//         boxShadow: theme.shadows[5],
+//         padding: theme.spacing(2, 4, 3),
+//     },
+// }));
 
 const NewFile = () => {
-    const classes = useStyles();
-    const [modalStyle] = useState(getModalStyle);
+    // const classes = useStyles();
+    // const [modalStyle] = useState(getModalStyle);
     const [open, setOpen] = useState(false);
     const [file, setFile] = useState(null);
     const [uploading, setUploading] = useState(false);
