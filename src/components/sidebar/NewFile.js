ac26e9432e7282b02501d7dadef9b6d32fd98e92
SHIVENDRA8004 <jat13632@gmail.com>
2023-08-18 09:58:35 +0530

    Feat: getting Lot of errors due to Old dependencies versions

--- src/components/sidebar/NewFile.js
+++ src/components/sidebar/NewFile.js
@@ -1,41 +1,83 @@
 import React, { useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
 import "../../styles/NewFile.css";
 
-// import { storage, db } from "../../firebase";
+import firebase from 'firebase/app';
+import 'firebase/storage'; // Import storage module
+import 'firebase/firestore'; // Import firestore module
+import { storageRef, db } from "../../firebase";
 
-// import { makeStyles } from "@mui/styles";
-// import Modal from "@mui/material";
-
-// function getModalStyle() {
-//     return {
-//         top: `50%`,
-//         left: `50%`,
-//         transform: `translate(-50%,-50%)`,
-//     };
-// }
-// const useStyles = makeStyles((theme) => ({
-//     paper: {
-//         position: "absolute",
-//         width: 400,
-//         backgroundColor: theme.palette.background.paper,
-//         border: `2px solid #000`,
-//         boxShadow: theme.shadows[5],
-//         padding: theme.spacing(2, 4, 3),
-//     },
-// }));
-
+const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    boxShadow: 5,
+    p: 4,
+    borderRadius: "8px",
+};
 const NewFile = () => {
-    // const classes = useStyles();
-    // const [modalStyle] = useState(getModalStyle);
     const [open, setOpen] = useState(false);
     const [file, setFile] = useState(null);
     const [uploading, setUploading] = useState(false);
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+    const handleChange = (e) => {
+        if (e.target.files[0]) {
+            setFile(e.target.files[0]);
+        }
+    };
+    const handleUpload = () => {
+        setUploading(true);
+        storageRef
+            .child(`files/${file.name}`)
+            .put(file)
+            .then((snapshot) => {
+                console.log(snapshot);
+                storageRef
+                    .child("files")
+                    .child(file.name)
+                    .getDownloadURL()
+                    .then((url) => {
+                        // Fixed typo "thrn" to "then"
+                        db.collection("myFiles").add({
+                            timestamp: firebase.firestore.FieldValue.serverTimestamp(), // Fixed reference to firestore.FieldValue
+                            caption: file.name,
+                            fileUrl: url,
+                            size: snapshot._delegate.bytesTransferred,
+                        });
+                        setUploading(false);
+                        setOpen(false);
+                        setFile(null);
+                    });
+            });
+    };
+
     return (
         <div className="newFile">
-            <div className="newFile_container">
+            <div className="newFile_container" onClick={handleOpen}>
                 <AddIcon />
                 <p>New</p>
+            </div>
+            <div>
+                <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+                    <Box sx={style}>
+                        <p>Select files you want to upload!</p>
+                        {uploading ? (
+                            <p>Uploading...</p>
+                        ) : (
+                            <>
+                                <input type="file" onChange={handleChange} />
+                                <button onClick={handleUpload}>Upload</button>
+                            </>
+                        )}
+                    </Box>
+                </Modal>
             </div>
         </div>
     );
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,9 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { GoogleAuthProvider } from "firebase/auth";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCoILFjg1FS7IrP85Fly9lbKu9QTkvG3GI",
     authDomain: "drive-project-d5b85.firebaseapp.com",
@@ -10,10 +12,12 @@ const firebaseConfig = {
     messagingSenderId: "312642396494",
     appId: "1:312642396494:web:610eacadd7cdbe55ff7e1f",
 };
+
 const firebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth();
+
+const auth = getAuth(firebaseApp);
+const db = getFirestore(firebaseApp);
+const storage = getStorage(firebaseApp);
 const provider = new GoogleAuthProvider();
-const storage = getStorage();
-const db = getFirestore();
 
 export { auth, provider, db, storage };
