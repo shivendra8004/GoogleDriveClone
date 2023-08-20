23e5c5772654b8f249bf4bdc842babf7cf3b09bb
SHIVENDRA8004 <jat13632@gmail.com>
2023-08-20 12:03:23 +0530

    New: Adding new sidebar in right side for icons of other apps

---
+++ src/components/sideIcons/SideIcons.js
@@ -0,0 +1,23 @@
+import React from "react";
+import AddIcon from "@mui/icons-material/Add";
+import CalenderIcon from "../../media/googlecalenderIcon.svg";
+import KeepIcon from "../../media/googlekeepIcon.svg";
+import TranslateIcon from "../../media/googletranslateIcon.svg";
+
+const SideIcons = () => {
+    return (
+        <div className="sideIcons">
+            <div className="sideIcons_top">
+                <img src={CalenderIcon} alt="Google Calender" />
+                <img src={KeepIcon} alt="Google Keep" />
+                <img src={TranslateIcon} alt="Google Translate" />
+            </div>
+            <hr />
+            <div className="sideIcons_plusIcon">
+                <AddIcon />
+            </div>
+        </div>
+    );
+};
+
+export default SideIcons;
