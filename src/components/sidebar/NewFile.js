a395b5eb01696f69b5e0614d0ad339fe93e9f4e3
SHIVENDRA8004 <jat13632@gmail.com>
2023-08-21 18:02:31 +0530

    New: Added styling in modal of file uploading

--- src/components/sidebar/NewFile.js
+++ src/components/sidebar/NewFile.js
@@ -8,16 +8,18 @@ import "../../styles/NewFile.css";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore/lite";
 import { storage, dbCollection } from "../../firebase";
-
+import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import CircularProgress from "@mui/material/CircularProgress";
 const style = {
     position: "absolute",
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
-    width: 400,
+    width: 500,
+    height: 300,
     bgcolor: "background.paper",
     boxShadow: 5,
-    p: 4,
     borderRadius: "8px",
 };
 const NewFile = ({ state, setState, userEmail }) => {
@@ -82,15 +84,71 @@ const NewFile = ({ state, setState, userEmail }) => {
             <div>
                 <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
                     <Box sx={style}>
-                        <p>Select files you want to upload!</p>
-                        {uploading ? (
-                            <p>Uploading...</p>
-                        ) : (
-                            <>
-                                <input type="file" multiple onChange={handleChange} />
-                                <button onClick={handleUpload}>Upload</button>
-                            </>
-                        )}
+                        <div
+                            style={{
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "space-between",
+                                borderBottom: "1px solid #000000",
+                                padding: "15px 30px 5px 30px",
+                            }}
+                        >
+                            <p style={{ margin: "0", fontSize: "1.3rem", fontWeight: "550" }}>Upload File</p>
+                            <CloseOutlinedIcon style={{ cursor: "pointer" }} onClick={handleClose} />
+                        </div>
+                        <div style={{ padding: "15px 30px" }}>
+                            <form
+                                style={{
+                                    border: "2px dashed #0066db",
+                                    borderRadius: "8px",
+                                    width: "100%",
+                                    height: "170px",
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                }}
+                            >
+                                {uploading ? (
+                                    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+                                        <CircularProgress />
+                                        <p> Uploading...</p>
+                                    </Box>
+                                ) : (
+                                    <div className="input_div">
+                                        <input type="file" className="files" multiple onChange={handleChange} id="files" name="files" />
+                                        <label htmlFor="files" className="files_icon">
+                                            <CloudUploadIcon style={{ color: "#0066db", fontSize: "2.5rem" }} />
+                                        </label>
+                                        <label className="files_text" htmlFor="files">
+                                            Select a file to Upload
+                                        </label>
+                                    </div>
+                                )}
+                            </form>
+                            <div className="" style={{ width: "100%", display: "flex", justifyContent: "center", marginTop: "15px" }}>
+                                <button
+                                    onClick={handleUpload}
+                                    style={{
+                                        display: "block",
+                                        height: "35px",
+                                        width: "70px",
+                                        backgroundColor: "transparent",
+                                        border: "1px solid #0066db",
+                                        borderRadius: "8px",
+                                        color: "#0066db",
+                                        fontWeight: "550",
+                                    }}
+                                >
+                                    {uploading ? (
+                                        <Box sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                                            <CircularProgress style={{ height: "20px", width: "20px" }} />
+                                        </Box>
+                                    ) : (
+                                        "Upload"
+                                    )}
+                                </button>
+                            </div>
+                        </div>
                     </Box>
                 </Modal>
             </div>
